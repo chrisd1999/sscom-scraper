abc72d848b4d1ea7767b246e62ed42e19b3f5a23
Kristers <45801982+chrisd1999@users.noreply.github.com>
2021-07-11 23:54:09 +0300

    Add custom command to scrape all last 2 days advertisements

---
+++ app/Console/Commands/ScrapeCommand.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Http\Controllers\AdvertisementController;
+use Illuminate\Console\Command;
+
+class ScrapeCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'scrape:all';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Scrape all last 2 days adverts';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        (new AdvertisementController)->store();
+        return 0;
+    }
+}
