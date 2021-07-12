78fc32043887f06be24098e9e9735d61331b0eca
Kristers <45801982+chrisd1999@users.noreply.github.com>
2021-07-12 12:58:22 +0300

    Add more constants urls, to scrape

--- config/constants.php
+++ config/constants.php
@@ -2,8 +2,17 @@
 
 return [
     'url' => [
-        'motorcycles' => 'https://www.ss.com/lv/transport/moto-transport/motorcycles/',
-        'last_2days'  => 'https://www.ss.com/lv/transport/moto-transport/motorcycles/today-2/',
-        'last_5days'  => 'https://www.ss.com/lv/transport/moto-transport/motorcycles/today-5/',
+        'motorcycles' => [
+            'brand_urls' => [
+                'honda'    => 'https://www.ss.lv/lv/transport/moto-transport/motorcycles/honda/',
+                'yamaha'   => 'https://www.ss.lv/lv/transport/moto-transport/motorcycles/yamaha/',
+                'suzuki'   => 'https://www.ss.lv/lv/transport/moto-transport/motorcycles/suzuki/',
+                'kawasaki' => 'https://www.ss.lv/lv/transport/moto-transport/motorcycles/kawasaki/',
+                'bmw'      => 'https://www.ss.lv/lv/transport/moto-transport/motorcycles/bmw/',
+                'ktm'      => 'https://www.ss.lv/lv/transport/moto-transport/motorcycles/ktm/',
+            ],
+            'last_2days' => 'https://www.ss.com/lv/transport/moto-transport/motorcycles/today-2/',
+            'last_5days' => 'https://www.ss.com/lv/transport/moto-transport/motorcycles/today-5/',
+        ]
     ],
 ];
