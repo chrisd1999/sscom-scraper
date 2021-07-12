dc798f61ee7ed44ab9304b2f9aa5d7947cec6f80
Kristers <45801982+chrisd1999@users.noreply.github.com>
2021-07-12 16:33:45 +0300

    Update constants to use sell in a link

--- config/constants.php
+++ config/constants.php
@@ -4,15 +4,15 @@ return [
     'url' => [
         'motorcycles' => [
             'brand_urls' => [
-                'honda'    => 'https://www.ss.lv/lv/transport/moto-transport/motorcycles/honda/',
-                'yamaha'   => 'https://www.ss.lv/lv/transport/moto-transport/motorcycles/yamaha/',
-                'suzuki'   => 'https://www.ss.lv/lv/transport/moto-transport/motorcycles/suzuki/',
-                'kawasaki' => 'https://www.ss.lv/lv/transport/moto-transport/motorcycles/kawasaki/',
-                'bmw'      => 'https://www.ss.lv/lv/transport/moto-transport/motorcycles/bmw/',
-                'ktm'      => 'https://www.ss.lv/lv/transport/moto-transport/motorcycles/ktm/',
+                'Honda'    => 'https://www.ss.lv/lv/transport/moto-transport/motorcycles/honda/sell/',
+                'Yamaha'   => 'https://www.ss.lv/lv/transport/moto-transport/motorcycles/yamaha/sell/',
+                'Suzuki'   => 'https://www.ss.lv/lv/transport/moto-transport/motorcycles/suzuki/sell/',
+                'Kawasaki' => 'https://www.ss.lv/lv/transport/moto-transport/motorcycles/kawasaki/sell/',
+                'BMW'      => 'https://www.ss.lv/lv/transport/moto-transport/motorcycles/bmw/sell/',
+                'KTM'      => 'https://www.ss.lv/lv/transport/moto-transport/motorcycles/ktm/sell/',
             ],
-            'last_2days' => 'https://www.ss.com/lv/transport/moto-transport/motorcycles/today-2/',
-            'last_5days' => 'https://www.ss.com/lv/transport/moto-transport/motorcycles/today-5/',
+            'last_2days' => 'https://www.ss.com/lv/transport/moto-transport/motorcycles/today-2/sell/',
+            'last_5days' => 'https://www.ss.com/lv/transport/moto-transport/motorcycles/today-5/sell/',
         ]
     ],
 ];
